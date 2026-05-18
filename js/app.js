82ac4e328375925de741348d1f8fe50d15420ff0
accessnetjatimulya <accessnetjatimulya@gmail.com>
2026-05-18 17:17:21 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
+const API_URL =
+'https://script.google.com/macros/s/AKfycbwUCq3J9slMtr-YPw_cW0CNQEJnmZy-UQyKG-EH0ZVmIS4xTbfyK_VXS5cGgJr_7TP4/exec';
 
+async function login(){
+
+  const username =
+    document.getElementById(
+      'username'
+    ).value;
+
+  const password =
+    document.getElementById(
+      'password'
+    ).value;
+
+  const res = await fetch(API_URL,{
+
+    method:'POST',
+
+    body:JSON.stringify({
+      action:'loginAdmin',
+      username:username,
+      password:password
+    })
+
+  });
+
+  const data =
+    await res.json();
+
+  if(data.status){
+
+    localStorage.setItem(
+      'admin',
+      JSON.stringify(data.user)
+    );
+
+    window.location =
+      'admin.html';
+
+  }else{
+
+    alert(data.message);
+
+  }
+
+}
